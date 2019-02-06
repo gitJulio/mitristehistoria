afeb0ee89d80f1c91c96ee8772a44089422448fc
Julio Bueso <julio.ezequiel@gmail.com>
2019-02-06 13:59:00 -0600

    Sitio Mi Triste Historia

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var app = express()
 
-app.get('/', (req, res) => {
-  let salida = {
-    "respuesta": true
-  }
-  res.send(salida)
-})
+app.use(express.static(__dirname + '/public'))
+
+// app.get('/', (req, res) => {
+//   let salida = {
+//     "respuesta": true
+//   }
+//   res.send(salida)
+// })
 
 app.listen(5000, () => {
   console.log("Api Mi Triste Historia is up");
