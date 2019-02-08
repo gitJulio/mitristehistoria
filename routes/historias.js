6e28b8134d51199fcf1eac6f1fa1f5f218a62bfa
Julio Bueso <julio.ezequiel@gmail.com>
2019-02-08 17:33:08 -0600

    Fecha

--- routes/historias.js
+++ routes/historias.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
 const pg = require('../config/ps_conection')
+
+var datetime = require('node-datetime');
+var dt;
+var formattedDate;
+
 //**************************************************//
 //**************************************************//
 app.get('/', async (req, res) => {
   let view_historias = await pg.func('public.ft_view_historias').catch(err => {
     console.log(err);
   })
-  console.log(view_historias);
+  dt = datetime.create(view_historias[0]["fecha"]);
+  formattedDate = dt.format('m/d/y H:M');
+
+  view_historias[0]["fecha"] = formattedDate
   res.render('home.hbs', {
     historias: view_historias
   })
@@ -21,7 +29,7 @@ app.get('/about', async (req, res) => {
     console.log(err);
   })
   // console.log(JSON.parse(view_historias));
-  console.log(view_historias.length);
+
   console.log("view_historias");
   // res.render('about.hbs')
   // res.send(view_historias)
