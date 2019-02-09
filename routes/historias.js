8d90b45f231d8572e293631ef2a287cf837be15c
Julio Bueso <julio.ezequiel@gmail.com>
2019-02-09 07:58:38 -0600

    Instalaciones

--- routes/historias.js
+++ routes/historias.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const pg = require('../config/ps_conection')
-
+var htmlencode = require('htmlencode');
 var datetime = require('node-datetime');
 var dt;
 var formattedDate;
-var misHistorias=new Array();
+var misHistorias = new Array();
+var decode = require('decode-html');
+
 
 //**************************************************//
 //**************************************************//
@@ -18,8 +20,11 @@ app.get('/', async (req, res) => {
   for (var i = 0; i < view_historias.length; i++) {
     dt = datetime.create(view_historias[i]["fecha"]);
     formattedDate = dt.format('m/d/y H:M');
-    view_historias[i]["fecha"]=formattedDate
+    view_historias[i]["fecha"] = formattedDate
   }
+
+
+  console.log();
 
 
   res.render('home.hbs', {
