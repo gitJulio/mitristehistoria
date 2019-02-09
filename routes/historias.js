21c2c7768a92f1e5045c2ff88911e91464fc620d
Julio Bueso <julio.ezequiel@gmail.com>
2019-02-09 08:08:01 -0600

    Rutas Historias clean

--- routes/historias.js
+++ routes/historias.js
@@ -16,16 +16,11 @@ app.get('/', async (req, res) => {
     console.log(err);
   })
 
-
   for (var i = 0; i < view_historias.length; i++) {
     dt = datetime.create(view_historias[i]["fecha"]);
     formattedDate = dt.format('m/d/y H:M');
     view_historias[i]["fecha"] = formattedDate
   }
-
-
-  console.log();
-
 
   res.render('home.hbs', {
     historias: view_historias
@@ -33,23 +28,6 @@ app.get('/', async (req, res) => {
 })
 
 
-
-app.get('/about', async (req, res) => {
-
-  let view_historias = await pg.func('public.ft_view_historias').catch(err => {
-    console.log(err);
-  })
-  // console.log(JSON.parse(view_historias));
-
-  console.log("view_historias");
-  // res.render('about.hbs')
-  // res.send(view_historias)
-})
-
-app.post('/mensajes', function(req, res) {
-  console.log(req.body.id)
-  res.json('get Mensaje');
-})
 
 //************************************************//
 //************************************************//
